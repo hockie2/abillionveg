6c0feea377b34161da1e0c50d617e8451be2fde3
Teo Boon Hock <hockie2@yahoo.com.sg>
2019-10-14 14:39:52 +0800

    first test

---
+++ recipes.js
@@ -0,0 +1,25 @@
+const recipes = [
+    {
+        "created_at": "Mon Aug 26 23:00:35 +0000 2019",
+        "id": 1166123304375054336,
+        "id_str": "1166123304375054336",
+        "text": "About 5 mins from flight attempt https://t.co/CWkvsqDQYE",
+        "truncated": false,
+        "entities": {
+          "hashtags": [],
+          "symbols": [],
+          "user_mentions": [],
+          "urls": [
+              {
+                  "url": "https://t.co/CWkvsqDQYE",
+                  "expanded_url": "https://twitter.com/spacex/status/1166120736538288129",
+                  "display_url": "twitter.com/spacex/status/…",
+                  "indices": [
+                  33,
+                  56
+                  ]
+              }
+          ]
+        }
+    }
+]
---
+++ script.jsx
@@ -0,0 +1,27 @@
+
+class List extends React.Component {
+                render() {
+                    // console.log("inside of component:", this.props.items );
+
+
+
+                    // console.log( "items elemetns",itemsElements );
+                    return (
+                      <ul>
+                        <p>TEST TEST</p>
+                      </ul>
+                    );
+                }
+            }
+
+
+
+
+
+ReactDOM.render(
+    <List/>,
+    document.getElementById('root')
+);
+
+
+
